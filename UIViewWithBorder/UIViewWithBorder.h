72e5242d0e706330aa639ae99e9ebea4ad70fdd8
andforce <86118@163.com>
2018-04-06 15:51:51 +0800

    init

---
+++ UIViewWithBorder/UIViewWithBorder.h
@@ -0,0 +1,19 @@
+//
+//  UIViewWithBorder.h
+//  UIViewWithBorder
+//
+//  Created by 迪远 王 on 2018/4/6.
+//  Copyright © 2018年 andforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIViewWithBorder.
+FOUNDATION_EXPORT double UIViewWithBorderVersionNumber;
+
+//! Project version string for UIViewWithBorder.
+FOUNDATION_EXPORT const unsigned char UIViewWithBorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewWithBorder/PublicHeader.h>
+
+
